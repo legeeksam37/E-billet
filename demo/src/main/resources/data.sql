7a665fb41aa9acfd0978b1fe4dab4912abac86cb
Yahya <legeeksam37@gmail.com>
2021-04-22 14:00:03 +0200

    1stCommit

---
+++ demo/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO PRODUCT VALUES(1, 'Ordinateur portable' , 350, 120);
+INSERT INTO PRODUCT VALUES(2, 'Aspirateur Robot' , 500, 200);
+INSERT INTO PRODUCT VALUES(3, 'Table de Ping Pong' , 750, 400);
